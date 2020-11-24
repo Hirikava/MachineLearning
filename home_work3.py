1d3b359b60cb82e0083efddf30cdbf78ff3217d9
AceSpade <313121@mail.ru>
2020-11-24 18:11:17 +0500

    Нифига полезного не сделал

--- home_work3.py
+++ home_work3.py
@@ -26,21 +26,14 @@ class MonteCarloAgent:
         self.number_of_states = dimensions[0].number * dimensions[1].number * dimensions[2].number * dimensions[3].number
         self.states = np.zeros((self.number_of_states,2))
 
-    # def get_state(self, observation):
-    #     state = math.floor((observation[0] + self.dimensions[0].offset) / self.dimensions[0].max * self.dimensions[0].number)
-    #     state += math.floor(sigma(observation[1]) * self.dimensions[1].number) * self.dimensions[0].number
-    #     state += math.floor(
-    #         (observation[2] + self.dimensions[2].offset) / self.dimensions[2].max * self.dimensions[2].number) * \
-    #              self.dimensions[0].number * self.dimensions[1].number
-    #     state += math.floor(sigma(observation[3]) * self.dimensions[3].number) * self.dimensions[0].number * self.dimensions[
-    #         1].number * self.dimensions[2].number
-    #     return int(state)
-
     def get_state(self, observation):
-        state = math.floor(sigma(observation[0]) * self.dimensions[0].number)
-        state += math.floor(sigma(observation[1]) * self.dimensions[1].number) * self.dimensions[0].number
-        state += math.floor(sigma(observation[2]) * self.dimensions[2].number) * self.dimensions[0].number * self.dimensions[1].number
-        state += math.floor(sigma(observation[3]) * self.dimensions[3].number) * self.dimensions[0].number * self.dimensions[1].number * self.dimensions[2].number
+        state = math.floor((observation[0] + self.dimensions[0].offset) / self.dimensions[0].max * self.dimensions[0].number)
+        state += math.floor((observation[1] + self.dimensions[1].offset) / self.dimensions[1].max * self.dimensions[1].number) * self.dimensions[0].number
+        state += math.floor(
+            (observation[2] + self.dimensions[2].offset) / self.dimensions[2].max * self.dimensions[2].number) * \
+                 self.dimensions[0].number * self.dimensions[1].number
+        state += math.floor((observation[3] + self.dimensions[3].offset) / self.dimensions[3].max * self.dimensions[3].number) * self.dimensions[0].number * self.dimensions[
+            1].number * self.dimensions[2].number
         return int(state)
 
     def make_action(self, state, epsilon):
@@ -67,9 +60,11 @@ class MonteCarloAgent:
 
     def get_session(self, session_length, environment, epsilon, render=False):
         observation = environment.reset()
-        states, actions, rewards = list(), list(), list()
+        states, actions, rewards, v1, v2 = list(), list(), list(), list(), list()
 
         for i in range(session_length):
+            v1.append(observation[1])
+            v2.append(observation[2])
             state = self.get_state(observation)
             action = self.make_action(state, epsilon)
             observation, reward, done, _ = environment.step(action)
@@ -85,34 +80,39 @@ class MonteCarloAgent:
             "actions": actions,
             "rewards": rewards,
             "total_reward": sum(rewards)
+
         }
 
-dis = 11
-dimensions = (Dimension(9.6, 4.8, dis), Dimension(1, 0, dis), Dimension(0.936, 0.418, dis), Dimension(1, 0, dis))
+dis = 7
+dimensions = (Dimension(9.6, 4.8, dis), Dimension(10, 5, dis*2), Dimension(0.936, 0.418, dis*4), Dimension(10, 5, dis*2))
 agent = MonteCarloAgent(dimensions)
 environment = gym.make("CartPole-v0")
 
-episode_count = 10000
-clear_episode_count = 1000
+episode_count = 2000
+clear_episode_count = 100
 rewards = list()
+session_length = 200
 
-epsilon = 0.1
+epsilon = 1
 d_epsilon = epsilon / episode_count
+
 for i in range(episode_count):
-    session = agent.get_session(500, environment, epsilon)
+    print(str(i) + '/' + str(episode_count))
+    session = agent.get_session(session_length, environment, epsilon)
     agent.update_policy(session, 0.99)
-    epsilon -= d_epsilon
+    epsilon = max(epsilon - d_epsilon, 0)
     rewards.append(session["total_reward"])
 
 for i in range(clear_episode_count):
-    session = agent.get_session(500, environment, 0)
-    agent.update_policy(session, 0.99)
+    print(str(i) + '/' + str(clear_episode_count) + " clear")
+    session = agent.get_session(session_length, environment, 0)
+    #agent.update_policy(session, 0.99)
     rewards.append(session["total_reward"])
 
 plot = pl.plot(range(episode_count + clear_episode_count), rewards)
 pl.show()
 
 for i in range(10):
-    session = agent.get_session(500, environment, 0, True)
+    session = agent.get_session(session_length, environment, 0, True)
     agent.update_policy(session, 0.99)
     rewards.append(session["total_reward"])
